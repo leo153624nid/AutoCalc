6fa519331d4865cc470936ef3e687575ea6b347b
leo153624nid <barjastroy@gmail.com>
2022-05-31 18:48:49 +0300

    css update

--- src/components/CarBtns/CarBtns.js
+++ src/components/CarBtns/CarBtns.js
@@ -4,7 +4,17 @@ import s from './CarBtns.module.css';
 function CarBtns () {
     return (
         <div className={s.CarBtns}>
-            
+            <div className={s.fuel}>
+                + fuel
+            </div>
+
+            <div className={s.etc}>
+                + etc
+            </div>
+
+            <div className={s.change}>
+                Редактировать
+            </div>
         </div>
     );
 }
--- src/components/CarCard/CarCard.js
+++ src/components/CarCard/CarCard.js
@@ -3,23 +3,32 @@ import s from './CarCard.module.css';
 
 
 function CarCard (props) {
-    console.dir(props);
+  
     const carPic = props.carPic;
 
     return (
         <div className={s.CarCard}>
             <img src={carPic} alt='CarPic'/>
 
-            <div>
-                name, distance
+            <div className={s.CarName}>
+                <span className={s.pad}>BMW 316i</span>
+                <span className={s.pad}>Пробег <span>XXXXXX</span> км.</span>
             </div>
+            
+            <div className={s.CarConsumptions}>
+                <div className={s.FuelConsumptions}>
+                    <span className={s.pad}>Расход топлива</span>
+                    <span className={s.pad}> <span>XX,X</span> л/100 км</span>
+                </div>
 
-            <div>
-                расход топлива, содержание
+                <div className={s.EtcConsumptions}>
+                    <span className={s.pad}>Содержание</span>
+                    <span className={s.pad}> <span>XXXXXX</span> руб/мес.</span>
+                </div>
             </div>
-
-            <div>
-                кнопка выбора
+            
+            <div className={s.CarTake}>
+                <span>Выбрать</span>
             </div>
         </div>
     );
