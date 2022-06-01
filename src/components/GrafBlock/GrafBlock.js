c45c917434881cec4963d07c71052c1724006681
leo153624nid <barjastroy@gmail.com>
2022-06-01 17:11:00 +0300

    css upd

--- src/components/GrafBlock/GrafBlock.js
+++ src/components/GrafBlock/GrafBlock.js
@@ -5,7 +5,39 @@ import s from './GrafBlock.module.css';
 function GrafBlock (props) {
     return (
         <div className={s.GrafBlock}>
-           
+            <div className={s.grafikName}>
+                Здесь будут названия графиков или измеряемая величина
+            </div>
+
+            <div className={s.grafik}>
+                Здесь будет график
+            </div>
+
+            <aside className={s.aside}>
+                <div className={s.generalDistance}>
+                    <span>Общий пробег</span>
+                    <span>XXXXXX км.</span>
+                </div>
+
+                <div className={s.generalFuel}>
+                    <span>Общий расход топлива</span>
+                    <span>XXX л./100 км.</span>
+                </div>
+
+                <div className={s.generalCostOneKm}>
+                    <span>Стоимость 1 км.</span>
+                    <span>XXXX,Х км.</span>
+                </div>
+
+                <div className={s.generalCostOneDay}>
+                    <span>Стоимость владения</span>
+                    <span>XXXХ руб./день</span>
+                </div>
+            </aside>
+
+            <div className={s.toggle}>
+                Здесь будут переключатели
+            </div>
         </div>
     );
 }
--- src/components/UpperBlock/UpperBlock.js
+++ src/components/UpperBlock/UpperBlock.js
@@ -5,7 +5,29 @@ import s from './UpperBlock.module.css';
 function UpperBlock (props) {
     return (
         <div className={s.UpperBlock}>
-           
+            <div className={s.moneyForFuel}>
+                <span>Расходы на топливо</span>
+            </div>
+
+            <div className={s.averageDistance}>
+                <span>Средний пробег</span>
+            </div>
+
+            <div className={s.averageConsumption}>
+                <span>Средний расход</span>
+            </div>
+
+            <div className={s.costFuel}>
+                <span>Стоимость топлива</span>
+            </div>
+
+            <div className={s.moneyForEtc}>
+                <span>Остальные расходы</span>
+            </div>
+
+            <div className={s.car}>
+                <span>carName</span>
+            </div>
         </div>
     );
 }
