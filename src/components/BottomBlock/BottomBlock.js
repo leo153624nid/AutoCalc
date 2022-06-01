2c784931a88d9b290bdc147c1fba6cb40500b13a
leo153624nid <barjastroy@gmail.com>
2022-06-01 17:29:18 +0300

    init BottomBlock

--- src/components/BottomBlock/BottomBlock.js
+++ src/components/BottomBlock/BottomBlock.js
@@ -1,11 +1,28 @@
 import React from "react";
 import s from './BottomBlock.module.css';
 
-
 function BottomBlock (props) {
     return (
         <div className={s.BottomBlock}>
-     
+            <div className={s.distance}>
+                <span>Расстояние</span>
+                <span>XXXXX км.</span>
+            </div>
+        
+            <div className={s.fuel}>
+                <span>Топливо</span>
+                <span>XXXXX руб.</span>
+            </div>                
+                
+            <div className={s.etc}>
+                <span>Прочее</span>
+                <span>XXXXХX руб.</span>
+            </div>
+
+            <div className={s.itogo}>
+                <span>Итого</span>
+                <span>XXXXХX руб.</span>
+            </div>
         </div>
     );
 }
