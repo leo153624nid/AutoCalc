2676ef039b5319eb8d47c6c45823743803f36704
leo153624nid <barjastroy@gmail.com>
2022-06-03 17:23:14 +0300

    Update Graf.js

--- src/components/Graf/Graf.js
+++ src/components/Graf/Graf.js
@@ -6,19 +6,21 @@ import BottomBlock from './BottomBlock/BottomBlock';
 import { useState } from 'react';
 
 function Graf (props) {
-    const [car, setCar] = useState(props.data);
+    const [grafik, setGrafik] = useState(1);
+    const car = props.data;
 
     function changeGrafik (key) {
         console.log(`grafik ${key}`);
+        setGrafik(+key);
     }
 
     return (
         <div className={s.Graf}>
             <UpperBlock changeGrafik={changeGrafik} carName={car.carName}/>
 
-            <GrafBlock />
+            <GrafBlock idGrafik={grafik} car={car}/>
 
-            <BottomBlock />
+            <BottomBlock car={car}/>
         </div>
     );
 }
