a225617000afff4aeecedb32453358827825a993
leo153624nid <barjastroy@gmail.com>
2022-06-04 10:57:04 +0300

    Update CarGrafik.js

--- src/components/Graf/GrafBlock/CarGrafik/CarGrafik.js
+++ src/components/Graf/GrafBlock/CarGrafik/CarGrafik.js
@@ -3,17 +3,15 @@ import s from './CarGrafik.module.css';
 
 
 function CarGrafik (props) {
+    const car = props.car;
+    const idGrafik = props.idGrafik;
     
 
 
     return (
- 
-
             <div className={s.grafik}>
                 Здесь будет график
             </div>
-
-           
     );
 }
 
