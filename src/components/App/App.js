98460689603904124d2bfa99c3ed7c5f8cac89a9
leo153624nid <barjastroy@gmail.com>
2022-06-08 20:37:07 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,6 +11,19 @@ import s from './App.module.css'
 
 function App(props) {
     const userCars = props.userData.userCars
+    const noCar = {
+        carId: 0,
+        carName: 'Нет машины',
+        carPic: 'NoCarImage',
+        distance: 0,
+        fuelConsumptions: 0,
+        etcConsumptions: 0,
+        allMonth: 0,
+        costOneKm: 0,
+        costOneDay: 0,
+        fuelings: [],
+        etc: [],
+    }
     const routeList = userCars.map((car) => (
         <Route
             key={car.carId}
@@ -25,8 +38,7 @@ function App(props) {
 
             <Routes>
                 <Route path="/" element={<Carusel userCars={userCars} />} />
-                {/* Пустое поле для графиков, если ошибка ДОДЕЛАТЬ */}
-                <Route path="/graf" element={<Graf />} />
+                <Route path="/graf" element={<Graf data={noCar} />} />
                 {routeList}
             </Routes>
 
