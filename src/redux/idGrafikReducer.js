1a3739b1864306fc1af188bbfe7e185f45154bce
leo153624nid <barjastroy@gmail.com>
2022-06-12 16:03:31 +0300

    Update idGrafikReducer.js

--- src/redux/idGrafikReducer.js
+++ src/redux/idGrafikReducer.js
@@ -1,11 +1,19 @@
 const CHANGE_GRAFIK = 'CHANGE_GRAFIK'
 
 const idGrafikReducer = (state, action) => {
-    if (action.type === CHANGE_GRAFIK) {
-        // eslint-disable-next-line no-param-reassign
-        state.idGrafik = +action.key
+    switch (action.type) {
+        case CHANGE_GRAFIK:
+            // eslint-disable-next-line no-param-reassign
+            state.idGrafik = Number(action.key)
+            return state.idGrafik
+        default:
+            return state.idGrafik
     }
-    return state.idGrafik
 }
+
+export const changeGrafikActionCreator = (value) => ({
+    type: CHANGE_GRAFIK,
+    key: value,
+})
 
 export default idGrafikReducer
