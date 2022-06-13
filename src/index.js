3541730b8bcf25a6513e7c80c8aabcbc4407fc2c
leo153624nid <barjastroy@gmail.com>
2022-06-13 13:06:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import App from './components/App/App'
 import store from './redux/reduxStore'
 
@@ -12,7 +13,13 @@ const rerenderAll = (state) => {
     root.render(
         <React.StrictMode>
             <Router>
-                <App state={state} dispatch={store.dispatch.bind(store)} />
+                <Provider store={store}>
+                    <App
+                        state={state}
+                        dispatch={store.dispatch.bind(store)}
+                        store={store}
+                    />
+                </Provider>
             </Router>
         </React.StrictMode>
     )
