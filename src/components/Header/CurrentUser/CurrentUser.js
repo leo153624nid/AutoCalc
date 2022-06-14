6c8f7ebea5d8c74f08749eb374d57c8b3701e18f
leo153624nid <barjastroy@gmail.com>
2022-06-14 20:16:30 +0300

    Update CurrentUser.js

--- src/components/Header/CurrentUser/CurrentUser.js
+++ src/components/Header/CurrentUser/CurrentUser.js
@@ -1,12 +1,15 @@
-import React from "react";
-import s from './CurrentUser.module.css';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import s from './CurrentUser.module.css'
 
-function CurrentUser () {
+function CurrentUser() {
     return (
         <div className={s.user}>
-            <span>Current User</span>
+            <NavLink to="/" className={s.btn}>
+                Current User
+            </NavLink>
         </div>
-    );
+    )
 }
 
-export default CurrentUser;
+export default CurrentUser
