f4972dbc8fb32a48edea816a34907d318e58e27b
leo153624nid <barjastroy@gmail.com>
2022-06-14 20:16:35 +0300

    Create CarDataInput.js

---
+++ src/components/NewCar/CarDataInput/CarDataInput.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import s from './CarDataInput.module.css'
+
+// eslint-disable-next-line react/prop-types
+function CarDataInput({ label }) {
+    return (
+        <div className={s.CarDataInput}>
+            <span className={s.pad}>
+                <b>{label}</b>
+            </span>
+            <input placeholder="1" />
+        </div>
+    )
+}
+
+export default CarDataInput
