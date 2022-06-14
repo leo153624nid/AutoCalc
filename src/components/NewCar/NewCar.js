81d675afcd9b8ece3767a732754396c3a5938703
leo153624nid <barjastroy@gmail.com>
2022-06-14 20:16:45 +0300

    Update NewCar.js

--- src/components/NewCar/NewCar.js
+++ src/components/NewCar/NewCar.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import s from './NewCar.module.css'
-import CarProp from './CarProp/CarProp'
+import CarDataInput from './CarDataInput/CarDataInput'
 import CarPreview from './CarPreview/CarPreview'
 
-function NewCar(props) {
+const carName = 'Название машины'
+const distance = 'Пробег'
+const yearProduction = 'Год производства'
+const yearOfBuying = 'Год покупки'
+const costOfBuying = 'Стоимость покупки'
+const vin = 'VIN номер'
+const notes = 'Заметки'
+
+function NewCar() {
     return (
         <div className={s.NewCar}>
-            <CarProp />
+            <div className={s.form}>
+                <CarDataInput label={carName} />
+                <CarDataInput label={distance} />
+                <CarDataInput label={yearProduction} />
+                <CarDataInput label={yearOfBuying} />
+                <CarDataInput label={costOfBuying} />
+                <CarDataInput label={vin} />
+                <CarDataInput label={notes} />
+            </div>
 
             <CarPreview />
         </div>
