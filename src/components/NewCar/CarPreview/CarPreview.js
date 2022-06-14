4a70e4ee26cb5f108e36a5cd012d9cc8ca33d51d
leo153624nid <barjastroy@gmail.com>
2022-06-14 20:16:39 +0300

    Update CarPreview.js

--- src/components/NewCar/CarPreview/CarPreview.js
+++ src/components/NewCar/CarPreview/CarPreview.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import s from './CarPreview.module.css'
 
-function CarPreview(props) {
+function CarPreview() {
     return <div className={s.CarPreview}>CarPreview</div>
 }
 
