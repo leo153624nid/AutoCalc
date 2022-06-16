b66c14b1d0e0009ecbd39a59078791badeaa95a0
leo153624nid <barjastroy@gmail.com>
2022-06-16 20:10:09 +0300

    Update reduxStore.js

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userDataReducer from './userDataReducer'
 import idGrafikReducer from './idGrafikReducer'
+import newCarReducer from './newCarReducer'
 
 const rootReducer = combineReducers({
     userData: userDataReducer,
     idGrafik: idGrafikReducer,
+    newCar: newCarReducer,
 })
 
 const store = configureStore({ reducer: rootReducer })
