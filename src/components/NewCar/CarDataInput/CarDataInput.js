0abcb1d918aca922474dda452d489cd31c91e7ad
leo153624nid <barjastroy@gmail.com>
2022-06-16 21:14:27 +0300

    Update CarDataInput.js

--- src/components/NewCar/CarDataInput/CarDataInput.js
+++ src/components/NewCar/CarDataInput/CarDataInput.js
@@ -2,13 +2,20 @@ import React from 'react'
 import s from './CarDataInput.module.css'
 
 // eslint-disable-next-line react/prop-types
-function CarDataInput({ label }) {
+function CarDataInput({ label, value, change }) {
     return (
         <div className={s.CarDataInput}>
             <span className={s.pad}>
                 <b>{label}</b>
             </span>
-            <input placeholder="введите значение" className={s.input} />
+            <input
+                onChange={(e) => {
+                    change(e.target.value)
+                }}
+                placeholder="введите значение"
+                className={s.input}
+                value={value}
+            />
         </div>
     )
 }
