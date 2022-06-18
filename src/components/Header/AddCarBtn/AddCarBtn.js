0c4fc8595594d8d10e725d37ab8aa6119c09e433
leo153624nid <barjastroy@gmail.com>
2022-06-18 19:03:31 +0300

    Update AddCarBtn.js

--- src/components/Header/AddCarBtn/AddCarBtn.js
+++ src/components/Header/AddCarBtn/AddCarBtn.js
@@ -1,10 +1,15 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react'
+import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import s from './AddCarBtn.module.css'
+import { clearCarAC } from '../../../redux/newCarReducer'
 
-function AddCarBtn() {
+function AddCarBtn({ сlearNewCar }) {
     return (
-        <div className={s.addCarBtn}>
+        <div className={s.addCarBtn} onClick={() => сlearNewCar()}>
             <NavLink to="/add_car" className={s.btn}>
                 + Добавить авто
             </NavLink>
@@ -12,4 +17,10 @@ function AddCarBtn() {
     )
 }
 
-export default AddCarBtn
+const mapDispatchToProps = (dispatch) => ({
+    сlearNewCar: () => {
+        dispatch(clearCarAC())
+    },
+})
+
+export default connect(null, mapDispatchToProps)(AddCarBtn)
