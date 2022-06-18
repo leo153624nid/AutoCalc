96ba1e19d3e6d5e2c1f6333dfe3e0c1d400292c3
leo153624nid <barjastroy@gmail.com>
2022-06-18 11:25:07 +0300

    add Middleware Logger

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,4 +1,6 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import logger from 'redux-logger'
 import userDataReducer from './userDataReducer'
 import idGrafikReducer from './idGrafikReducer'
 import newCarReducer from './newCarReducer'
@@ -11,6 +13,9 @@ const rootReducer = combineReducers({
     newFuel: newFuelReducer,
 })
 
-const store = configureStore({ reducer: rootReducer })
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+})
 
 export default store
