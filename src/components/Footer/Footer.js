010f24878887f69f33609f47976b1603acc5a0c3
leo153624nid <barjastroy@gmail.com>
2022-06-19 16:29:59 +0300

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,15 +5,64 @@ import { NavLink } from 'react-router-dom'
 import ModalContext from '../../contexts/ModalContext/ModalContext'
 import s from './Footer.module.css'
 
+const modalBodyAbout = (
+    <div>
+        <h4>Это учебное приложение для учета автомобильных расходов</h4>
+        <div>
+            <i>Вы можете:</i>
+        </div>
+        <div>
+            <ul>
+                <li>Добавлять данные о машинах</li>
+                <li>Редактировать данные о машинах</li>
+                <li>Удалять данные о машинах (пока не работает)</li>
+                <li>Добавлять данные о заправках и прочих расходах</li>
+                <li>
+                    Редактировать данные о заправках и прочих расходах (пока не
+                    работает)
+                </li>
+                <li>
+                    Удалять данные о заправках и прочих расходах (пока не
+                    работает)
+                </li>
+                <li>
+                    Получать статистику (графики и средние показатели) о
+                    расходах
+                </li>
+            </ul>
+        </div>
+    </div>
+)
+const modalBodySupport = (
+    <div>
+        <h4>telegram: @leo153624nid</h4>
+        <h4>
+            LinkedIn:{' '}
+            <a href="http://linkedin.com/in/alexey-chaykin-b229b323a">
+                alexey-chaykin
+            </a>
+        </h4>
+        <h4>
+            GitHub:{' '}
+            <a href="https://leo153624nid.github.io/chaykin_aleksey.github.io/">
+                leo153624nid
+            </a>
+        </h4>
+    </div>
+)
+
 function Footer() {
     const { openModal } = useContext(ModalContext)
 
     const handleClickAboutBtn = () => {
-        openModal({ title: 'О приложении' })
+        openModal({ title: 'О приложении', body: modalBodyAbout })
     }
 
     const handleClickSupportBtn = () => {
-        openModal({ title: 'Поддержка' })
+        openModal({
+            title: 'Поддержка',
+            body: modalBodySupport,
+        })
     }
 
     return (
