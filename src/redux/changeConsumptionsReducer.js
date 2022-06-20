3fe77489e77b50e3945ee75e8c1d211b0ff076a2
leo153624nid <barjastroy@gmail.com>
2022-06-20 20:13:29 +0300

    Update changeConsumptionsReducer.js

--- src/redux/changeConsumptionsReducer.js
+++ src/redux/changeConsumptionsReducer.js
@@ -13,94 +13,44 @@ const CHANGE_ETC_ID = 'CHANGE_ETC_ID'
 
 const changeConsumptionsReducer = createReducer(initialState, (builder) => {
     builder
-        .addCase(CLEAR_FUEL, () => ({
+        .addCase(CLEAR_CHANGING, () => ({
             ...initialState,
         }))
-        .addCase(SET_NEW_FUEL, (state, action) => ({
-            ...action.fuel,
+        .addCase(SET_NEW_CHANGING, (state, action) => ({
+            ...action.changing,
         }))
-        .addCase(CHANGE_DATE_FUEL, (state, action) => ({
+        .addCase(CHANGE_FUELING_ID, (state, action) => ({
             ...state,
-            date: action.date,
+            fuelingId: action.fuelingId,
         }))
-        .addCase(CHANGE_DISTANCE_FUEL, (state, action) => ({
+        .addCase(CHANGE_ETC_ID, (state, action) => ({
             ...state,
-            distance: action.distance,
-        }))
-        .addCase(CHANGE_MARK_FUEL, (state, action) => ({
-            ...state,
-            mark: action.mark,
-        }))
-        .addCase(CHANGE_PRICE_FUEL, (state, action) => ({
-            ...state,
-            price: action.price,
-        }))
-        .addCase(CHANGE_VOLUME_FUEL, (state, action) => ({
-            ...state,
-            volume: action.volume,
-        }))
-        .addCase(CHANGE_COST_FUEL, (state, action) => ({
-            ...state,
-            cost: action.cost,
-        }))
-        .addCase(CHANGE_FULL_FUEL, (state, action) => ({
-            ...state,
-            full: action.full,
+            etcId: action.etcId,
         }))
         .addDefaultCase((state) => state)
 })
 
-// Сбросить данные новой заправки
-export const clearFuelAC = () => ({
-    type: CLEAR_FUEL,
+// Сбросить данные об изменяемом обьекте
+export const clearChangingAC = () => ({
+    type: CLEAR_CHANGING,
 })
 
-// Обновить данные новой заправки
-export const setNewFuelAC = (fuel) => ({
-    type: SET_NEW_FUEL,
-    fuel,
+// Обновить данные об изменяемом обьекте
+export const setNewChangingAC = (changing) => ({
+    type: SET_NEW_CHANGING,
+    changing,
 })
 
-// Смена даты заправки
-export const changeDateFuelAC = (date) => ({
-    type: CHANGE_DATE_FUEL,
-    date: getDate(date),
+// Смена ID заправки
+export const changeFuelingIdAC = (fuelingId) => ({
+    type: CHANGE_FUELING_ID,
+    fuelingId: Number(fuelingId),
 })
 
-// Смена пробега заправки
-export const changeDistanceFuelAC = (distance) => ({
-    type: CHANGE_DISTANCE_FUEL,
-    distance: Number(distance),
-})
-
-// Смена марки топлива
-export const changeMarkFuelAC = (mark) => ({
-    type: CHANGE_MARK_FUEL,
-    mark,
-})
-
-// Смена цены топлива
-export const changePriceFuelAC = (price) => ({
-    type: CHANGE_PRICE_FUEL,
-    price: Number(price),
-})
-
-// Смена обьема топлива
-export const changeVolumeFuelAC = (volume) => ({
-    type: CHANGE_VOLUME_FUEL,
-    volume: Number(volume),
-})
-
-// Смена стоимости заправки
-export const changeCostFuelAC = (cost) => ({
-    type: CHANGE_COST_FUEL,
-    cost: Number(cost),
-})
-
-// Смена значения полного бака
-export const changeFullFuelAC = (full) => ({
-    type: CHANGE_FULL_FUEL,
-    full: Number(full),
+// Смена ID прочих расходов
+export const changeEtcIdAC = (etcId) => ({
+    type: CHANGE_ETC_ID,
+    etcId: Number(etcId),
 })
 
 export default changeConsumptionsReducer
