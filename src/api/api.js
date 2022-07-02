77eb43e6d75249dd72d81ef0026e051628d3dd90
leo153624nid <barjastroy@gmail.com>
2022-07-02 17:42:31 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,36 +1,52 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios'
 
-const baseUrl =
-    'https://autocalculato-default-rtdb.europe-west1.firebasedatabase.app/users/0'
+const axiosInstance = axios.create({
+    baseURL:
+        'https://autocalculato-default-rtdb.europe-west1.firebasedatabase.app/users/',
+})
 
-// Получить данные пользователя
-export const getUserData = () => axios.get(`${baseUrl}.json`)
-
-// Обновить или добавить машину
-export const patchUserCar = (car, index) =>
-    axios.patch(`${baseUrl}/userCars/${index}.json`, { ...car })
-
-// Удалить машину
-export const deleteUserCar = (index) =>
-    axios.delete(`${baseUrl}/userCars/${index}.json`)
-
-// Обновить или добавить заправку
-export const patchUserFuel = (fuel, carIndex, fuelIndex) =>
-    axios.patch(`${baseUrl}/userCars/${carIndex}/fuelings/${fuelIndex}.json`, {
-        ...fuel,
-    })
-
-// Удалить заправку
-export const deleteUserFuel = (carIndex, fuelIndex) =>
-    axios.delete(`${baseUrl}/userCars/${carIndex}/fuelings/${fuelIndex}.json`)
-
-// Обновить или добавить прочие расходы
-export const patchUserEtc = (etc, carIndex, etcIndex) =>
-    axios.patch(`${baseUrl}/userCars/${carIndex}/etc/${etcIndex}.json`, {
-        ...etc,
-    })
-
-// Удалить прочие расходы
-export const deleteUserEtc = (carIndex, etcIndex) =>
-    axios.delete(`${baseUrl}/userCars/${carIndex}/etc/${etcIndex}.json`)
+export const userDataAPI = {
+    // Получить данные пользователя
+    getUserData() {
+        return axiosInstance.get(`0.json`)
+    },
+    // Обновить или добавить машину
+    patchUserCar(car, index) {
+        return axiosInstance.patch(`0/userCars/${index}.json`, { ...car })
+    },
+    // Удалить машину
+    deleteUserCar(index) {
+        return axiosInstance.delete(`0/userCars/${index}.json`)
+    },
+    // Обновить или добавить заправку
+    patchUserFuel(fuel, carIndex, fuelIndex) {
+        return axiosInstance.patch(
+            `0/userCars/${carIndex}/fuelings/${fuelIndex}.json`,
+            {
+                ...fuel,
+            }
+        )
+    },
+    // Удалить заправку
+    deleteUserFuel(carIndex, fuelIndex) {
+        return axiosInstance.delete(
+            `0/userCars/${carIndex}/fuelings/${fuelIndex}.json`
+        )
+    },
+    // Обновить или добавить прочие расходы
+    patchUserEtc(etc, carIndex, etcIndex) {
+        return axiosInstance.patch(
+            `0/userCars/${carIndex}/etc/${etcIndex}.json`,
+            {
+                ...etc,
+            }
+        )
+    },
+    // Удалить прочие расходы
+    deleteUserEtc(carIndex, etcIndex) {
+        return axiosInstance.delete(
+            `0/userCars/${carIndex}/etc/${etcIndex}.json`
+        )
+    },
+}
