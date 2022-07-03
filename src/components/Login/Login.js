3809590e180ec716ec7ac623ba40a924789e2b58
leo153624nid <barjastroy@gmail.com>
2022-07-03 10:24:08 +0300

    Create Login.js

---
+++ src/components/Login/Login.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import s from './Login.module.css'
+
+function Login() {
+    return <div className={s.login}>LOGIN PAGE HERE</div>
+}
+
+export default Login
